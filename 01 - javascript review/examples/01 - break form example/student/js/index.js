827ea88497dd9c05f012db64eb99cbc015cc51c9
ovuong1 <ovuong1@nait.ca>
2026-01-14 11:31:05 -0700

    js review

--- 01 - javascript review/examples/01 - break form example/student/js/index.js
+++ 01 - javascript review/examples/01 - break form example/student/js/index.js
@@ -6,3 +6,37 @@ HTML for list topic list item
     NEW TOPIC HERE
 </li>
 */
+
+let topicList = document.querySelector('.topics-list');
+let newTopicForm = document.querySelector('.new-topic-form')
+
+// 5. function to add topic to page
+const addTopicToPage = (topicName, topicListElement) => {
+    // 6. create new list item element
+    let newTopicElement = `<li class="list-group-item">
+    ${topicName}
+    </li>`
+    topicListElement.innerHTML += newTopicElement
+    
+}
+
+// 2. add event listener
+newTopicForm.addEventListener(
+    "submit", 
+    (event) => { 
+        event.preventDefault()
+        // 3. get the value from the form input
+        let topicInput = event.target.elements["new-topic"]
+        let newTopic = topicInput.value
+        console.log(newTopic)
+        //4. validation - make sure its not empty
+        if (newTopic === "") {
+            topicInput.classList.add("is-invalid")
+        }
+        else {
+            topicInput.classList.remove("is-invalid")
+            
+     }
+    addTopicToPage(newTopic, topicList)
+})
+
