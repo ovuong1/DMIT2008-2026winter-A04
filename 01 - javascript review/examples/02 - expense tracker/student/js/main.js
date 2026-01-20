210e9ae25ba8e770ae9e5bc2c0887b6327544558
ovuong1 <ovuong1@nait.ca>
2026-01-20 09:25:25 -0700

    implement add/edit card

--- 01 - javascript review/examples/02 - expense tracker/student/js/main.js
+++ 01 - javascript review/examples/02 - expense tracker/student/js/main.js
@@ -1,12 +1,11 @@
 //Instruction On expense-tracker-code-explained.md
 import theExpenses from "./expense-data.js";
 
-//2. select the container elemnet thatour cards will be nested in 
+//2. select the container element that our cards will be nested in 
 const expenseContainer = document.getElementById("expense-container");
 
-//3. redner out the data as a agrid of cards
+//3. render out the data as a grid of cards
 function renderExpenses(expenses) {
-    const expenseList = document.getElementById("expense-list");
     expenseContainer.innerHTML = "";
 
      expenses.forEach((expense) => {
@@ -29,3 +28,51 @@ function renderExpenses(expenses) {
 }
 // 4. render the results 
 renderExpenses(theExpenses);
+    
+//5. implement add/edit behavior
+document
+  .getElementById("add-expense-form")
+  .addEventListener(
+    "submit", // the form event im looking for
+    function (event) {
+      event.preventDefault(); // prevent the default behavior of the form
+      const title = document.getElementById("title").value;
+      const amount = parseFloat(document.getElementById("amount").value);
+        const date = document.getElementById("date").value;
+        const category = document.getElementById("category").value;
+        // we're going to write this behavior to be reusable for edit later
+        // because we can use the inline text on the button to determine whether it's an add or edit
+        if(document.getElementById("submiter").innerText == "Add Expense")
+        {
+            if(title && category && date && !isNaN(amount))
+            {
+                //create a new expense object we'll be adding to the grid of cards
+                const newExpense = {
+                    id: theExpenses.length + 1,
+                    title,
+                   amount,
+                    date,
+                    category,
+                };
+                theExpenses.push(newExpense);
+                renderExpenses(theExpenses);
+                event.target.reset();
+            } else {
+                alert("Please fill in all fields correctly.");
+            }
+        } else {
+            // non obvious if the text isnt "Add Expense" then it must be "Edit Expense"
+            const expenseId = parseInt(document.getElementById("expense-id").value);
+            const expenseToEdit = theExpenses.find(
+                (expense) => expense.id === expenseId);
+           if(expenseToEdit) // simple null check 
+            {
+                expenseToEdit.title = title;
+                expenseToEdit.amount = amount;
+                expenseToEdit.date = date;
+                expenseToEdit.category = category;
+                this.reset();
+                renderExpenses(theExpenses); // re render data into cards 
+            }
+        }
+    })
